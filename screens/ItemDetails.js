56004e98b7ab7415d273252b21b890ec16e525f8
SHYLLA <shylla@live.com>
2017-10-08 09:55:09 +0700

    Fetch item descriptions from Store

--- screens/ItemDetails.js
+++ screens/ItemDetails.js
@@ -89,13 +89,10 @@ export default class ItemDetailsScreen extends React.Component {
             </Row>
 
             <Bold>Description</Bold>
-            <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
-            <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
+            {this.item.description.split('\n').map(desc => <Text key={desc}>{desc}</Text>)}
 
             <Bold style={{ marginTop: 16 }}>Terms & Conditions</Bold>
-            <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
-            <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
-
+            {this.item.terms.split('\n').map(desc => <Text key={desc}>{desc}</Text>)}
 
           </Container>
         </ScrollView>
--- screens/Milestones.js
+++ screens/Milestones.js
@@ -5,12 +5,12 @@ import BaseScreen from '../components/BaseScreen';
 
 import MilestonesMock from '../assets/mocks/milestones.jpg';
 
-export default class MilestonesScreen extends React.Components {
+export default class MilestonesScreen extends React.Component {
 	render() {
 		return (
 			<BaseScreen>
 				<ScrollView style={{ flex: 1 }} >
-					<Image source={MilestonesMock} /> 
+					<Image source={MilestonesMock} resizeMode="contain" /> 
 				</ScrollView>
 			</BaseScreen>
 		);
